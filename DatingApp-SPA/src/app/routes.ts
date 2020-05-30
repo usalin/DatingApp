de067cc1db2f9b85165aa52d3f2ceb9fad08615b
ugur salin <ugursalin@gmail.com>
2020-05-30 16:54:35 +0300

    BuiltUserInterface

--- DatingApp-SPA/src/app/routes.ts
+++ DatingApp-SPA/src/app/routes.ts
@@ -2,9 +2,12 @@
 import {Routes} from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ListsComponent } from './lists/lists.component';
-import { MemberListComponent } from './member-list/member-list.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
 import { MessagesComponent } from './messages/messages.component';
 import { AuthGuard } from './_guards/auth.guard';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { MemberDetailResolver } from './_resolvers/member-detail.resolver';
+import { MemberListResolver } from './_resolvers/member-list.resolver';
 
 export const appRoutes: Routes = [
 
@@ -16,7 +19,9 @@ export const appRoutes: Routes = [
     runGuardsAndResolvers: 'always',
     children: [
 
-        {path: 'members', component: MemberListComponent},
+        {path: 'members', component: MemberListComponent, resolve: {users: MemberListResolver}},
+        {path: 'members/:id', component: MemberDetailComponent, resolve: {user: MemberDetailResolver}},
+
         {path: 'messages', component: MessagesComponent},
         {path: 'lists', component: ListsComponent},
     ]
